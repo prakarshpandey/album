562c69c41e4939d6005cf3f4fa34e4616d9e90d1
prakarshpandey <ppandey@g.hmc.edu>
2017-06-25 22:52:29 -0700

    Added Header component with styles and other pretty things

--- index.android.js
+++ index.android.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Text, AppRegistry } from 'react-native'
+import { AppRegistry } from 'react-native'
+import Header from './src/components/Header'
 
 class App extends React.Component {
   render () {
     return (
-      <Text> Some text </Text>
+      <Header headerText={'Albums'}/>
     )
   }
 }
